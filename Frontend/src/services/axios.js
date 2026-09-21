98dc7b6493a7a69542591753e833784991293b96
Yogesh Vishwakarma <yvishwakarma603@gmail.com>
2026-09-21 18:28:52 +0530

    Configure frontend API URL for deployment

--- Frontend/src/services/axios.js
+++ Frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 
 export default axios;
