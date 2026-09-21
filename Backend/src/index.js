396601f28ab87660086395f88d4ab979a63d8ac0
Yogesh Vishwakarma <yvishwakarma603@gmail.com>
2026-09-21 20:28:03 +0530

    Fix production CORS

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:[
+    'http://localhost:5173',
+    "https://helpdesk-ticketing-frontend.vercel.app"
+  ],
   credentials: true 
 }))
 
